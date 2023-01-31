0bd4b8e80082c7d403f70b8407ac59233a114569
reality-comes <121299032+reality-comes@users.noreply.github.com>
2023-01-30 20:41:29 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -25,7 +25,8 @@ from src.utils import (
 from src import completion
 from src.completion import generate_completion_response, process_response
 from src.memory import (
-    gpt3_embedding, 
+    gpt3_embedding,
+    gpt3_response_embedding, 
     save_json, 
     load_convo,
     fetch_memories,
@@ -61,53 +62,6 @@ async def on_ready():
                 messages.append(m)
         completion.MY_BOT_EXAMPLE_CONVOS.append(Conversation(messages=messages))
     await tree.sync()
-
-
-# /chat message:
-@tree.command(name="chat", description="Talk to the bot")
-@discord.app_commands.checks.has_permissions(send_messages=True)
-@discord.app_commands.checks.has_permissions(view_channel=True)
-@discord.app_commands.checks.bot_has_permissions(send_messages=True)
-@discord.app_commands.checks.bot_has_permissions(view_channel=True)
-@discord.app_commands.checks.bot_has_permissions(manage_threads=True)
-async def chat_command(int: discord.Interaction, message: str, channel: discord.TextChannel):
-    try:
-        # block servers not in allow list
-        if should_block(guild=int.guild):
-            return
-
-        user = int.user
-        logger.info(f"Chat command by {user} {message[:20]}")
-        try:
-            embed = discord.Embed(
-                description=f"<@{user.id}> wants to chat! 🤖💬",
-                color=discord.Color.green(),
-            )
-            embed.add_field(name=user.name, value=message)
-            await int.response.send_message(embed=embed)
-            response = await int.original_response()
-        except Exception as e:
-            logger.exception(e)
-            await int.response.send_message(
-                f"Failed to start chat {str(e)}", ephemeral=True
-            )
-            return
-
-        async with channel.typing():
-            # fetch completion
-            messages = [Message(user=user.name, text=message)]
-            response_data = await generate_completion_response(
-                messages=messages, user=user
-            )
-            # send the result
-            await process_response(
-                user=user, channel=channel, response_data=response_data
-            )
-    except Exception as e:
-        logger.exception(e)
-        await int.response.send_message(
-            f"Failed to start chat {str(e)}", ephemeral=True
-        )
 
 
 # calls for each message
@@ -173,6 +127,15 @@ async def on_message(message: DiscordMessage):
             response_data = await generate_completion_response(
                 messages=channel_messages, user=message.author
             )
+            vector = gpt3_response_embedding(response_data)
+            timestamp = time()
+            timestring = timestring = timestamp_to_datetime(timestamp)
+            user = client.user.name
+            extracted_message = '%s: %s - %s' % (user, timestring, response_data.reply_text)
+            info = {'speaker': user, 'timestamp': timestamp,'uuid': str(uuid4()), 'vector': vector, 'message': extracted_message, 'timestring': timestring}
+            filename = 'log_%s_bot' % timestamp
+            save_json(f'./src/chat_logs/{filename}.json', info)
+
 
         # send response
         await process_response(
